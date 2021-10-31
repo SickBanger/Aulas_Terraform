4a6f60dbf402b4adfefd8584adbba9f2c32e80ce
SickBanger <darley_rom@hotmail.com>
2021-10-31 08:58:21 -0300

    AAdicionar versão do Terraform entre as linhas 1 a 11

--- Aula_01/main.tf
+++ Aula_01/main.tf
@@ -1,3 +1,16 @@
+terraform {
+  required_version = "0.14.4"
+
+  
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
+}
+
+ 
 provider "aws" {
 
   region = "região mais próxima"
